bc3c70dd4dc28b9ca3101b72dcf207f4aa2705bf
Infvmous <alexeyheather@gmail.com>
2020-10-07 18:02:56 +0700

    added license, updated requirements.txt, uploaded to pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
 
 setup(
     name='keitaropy',
     version='0.1',
-    packages=['keitaropy'],
+    packages=find_packages(),
     install_requires=[
         'requests'
     ],
@@ -13,15 +16,20 @@ setup(
     },
     author='Alex M',
     author_email='alexeyheather@gmail.com',
-    description='Python3 library based on Keitaro Admin API',
+    description='Simple and easy to use API wrapper library for Keitaro Admin API written in Python3 and requests',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/Infvmous/keitaropy',
-    keywords='keitaro admin api requests',
+    keywords='keitaro api requests api-wrapper api-client python python3 admin-api',
     project_urls={
-        'Source Code': 'https://github.com/Infvmous/keitaropy',
+        'Source Code': 'https://github.com/Infvmous/keitaropy/tree/master/keitaropy',
         'Documentation': 'https://github.com/Infvmous/keitaropy/blob/master/README.md',
         'Keitaro Admin API Documentation': 'https://admin-api.docs.keitaro.io/'
     },
     classifiers=[
-        "License :: OSI Approved :: Python Software Foundation License"
-    ]
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8'
 )
