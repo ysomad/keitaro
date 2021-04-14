ec6a9d0e6269a34b204cafae695f29d93d8c74e7
Alex Malykh <alexeyheather@gmail.com>
2021-04-14 18:16:22 +0700

    offers refactoring

--- keitaro/resources/offers.py
+++ keitaro/resources/offers.py
@@ -8,33 +8,51 @@ class Offer(API):
         super(Offer, self).__init__(client, endpoint)
 
     def get(self, offer_id=None):
-        """Getting all offers or specific on by its id"""
-        return super(Offer, self).get(offer_id)
+        """
+        Gets all offers or specific on by its id
+        """
+        return super(Offer, self).prepare_request('GET', offer_id)
 
     def download_landing(self, offer_id):
-        """Downloads local landing"""
-        return super(Offer, self).get(offer_id, 'download')
+        """
+        Downloads local landing
+        """
+        return super(Offer, self).prepare_request('GET', offer_id, 'download')
 
     def files_structure(self, offer_id):
-        """Returns files structure of local landing"""
-        return super(Offer, self).get(offer_id, 'get_structure')
+        """
+        Returns files structure of local landing
+        """
+        return super(Offer, self).prepare_request(
+            'GET', offer_id, 'get_structure')
 
     def get_file(self, offer_id, file_path):
-        """Getting file data of local landing"""
-        return super(Offer, self).get(offer_id, path=file_path)
+        """
+        Gets file data of local landing
+        """
+        return super(Offer, self).prepare_request(
+            'GET', offer_id, path=file_path)
 
     def create(self, name, *, group_id=None, offer_type=None,
                action_type=None, action_payload=None, archive=None,
                affiliate_network_id=None, payout_value=None, notes=None,
                payout_currency=None, payout_type=None, state=None,
                payout_auto=None, payout_upsell=None, country=None):
-        """Creating new offer"""
-        return super(Offer, self).post(**remove_key_values(locals()))
+        """
+        Creates new offer
+        """
+        return super(Offer, self).prepare_request(
+            'POST', **remove_key_values(locals()))
 
     def add_file(self, offer_id, file_path):
-        """Create file of local landing"""
-        return super(Offer, self).post(offer_id, 'add_file', path=file_path)
+        """
+        Creates file of local landing
+        """
+        return super(Offer, self).prepare_request(
+            'POST', offer_id, 'add_file', path=file_path)
 
     def clone(self, offer_id):
-        """Cloning offer by its id"""
-        return super(Offer, self).post(offer_id, 'clone')
+        """
+        Clones offer by its id
+        """
+        return super(Offer, self).prepare_request('POST', offer_id, 'clone')
