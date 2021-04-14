e285ece723203ceb3d9c8567dab2696cec5a6d2b
Alex Malykh <alexeyheather@gmail.com>
2021-04-14 18:21:10 +0700

    reports refactoring

--- keitaro/resources/reports.py
+++ keitaro/resources/reports.py
@@ -7,22 +7,28 @@ class Report(API):
         super(Report, self).__init__(client, endpoint)
 
     def get_labels(self, campaign_id, label_name, ref_name):
-        """Getting reports labels"""
-        return super(Report, self).get(
-            'labels', campaign_id=campaign_id,
+        """
+        Gets reports labels
+        """
+        return super(Report, self).prepare_request(
+            'GET', 'labels', campaign_id=campaign_id,
             label_name=label_name, ref_name=ref_name)
 
-    def build(self, interval, timezone,
-              metrics=['clicks', 'campaign_unique_clicks',
-                       'conversions', 'roi'], grouping=['campaign'],
-              sort={'name': 'clicks', 'order': 'DESC'}):
-        """Building custom keitaro report"""
-        return super(Report, self).post(
-            'build', range={'interval': interval, 'timezone': timezone},
+    def build(
+            self, interval, timezone,
+            metrics=['clicks', 'campaign_unique_clicks', 'conversions', 'roi'],
+            grouping=['campaign'], sort={'name': 'clicks', 'order': 'DESC'}):
+        """
+        Builds custom keitaro report
+        """
+        return super(Report, self).prepare_request(
+            'POST', 'build', range={'interval': interval, 'timezone': timezone},
             metrics=metrics, grouping=grouping, sort=[sort])
 
     def update_labels(self, campaign_id, ref_name, items):
-        """Update report labels"""
-        return super(Report, self).post(
-            'labels', campaign_id=campaign_id,
+        """
+        Updates report labels
+        """
+        return super(Report, self).prepare_request(
+            'POST', 'labels', campaign_id=campaign_id,
             ref_name=ref_name, items={'value': items})
